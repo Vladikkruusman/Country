7186ad14a6e9b4d98d6ed6664d4cde685affa35b
Vladikkruusman <vladkruusman213@gmail.com>
2020-11-04 23:24:22 +0200

    Idk why it doesn't work

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const axios= require('axios');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static('public'));
+app.set('view engine', ejs);
+app.get('/', (req, res)=> {
+    let url = 'https://restcountries.eu/rest/v2/name${country}?fullText=true';
+
+    axios.get(url)
+    .then(function(res){
+        let countryObject = res.data[0];
+        console.log(countryObject.name);
+        res.render('index.ejs', {countryObject: countryObject});
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+});
+
+app.post('/', (req, res)=> {
+    let country = req.body.country;
+    let url = 'https://restcountries.eu/rest/v2/name/${country}?fullText=true';
+
+    axios.get(url)
+    .then(function(res){
+        let countryObject = res.data[0];
+        res.render('index.ejs', {countryObject: countryObject});
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+
+ 
+});
+app.listen(5000, () => {
+    console.log("Server is running on Port 5000");
+});
